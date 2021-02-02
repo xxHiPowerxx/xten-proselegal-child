c97b6041ae79de16545355480a30d2742620363d
xxHiPowerxx <rickyadams432@gmail.com>
2021-02-02 11:24:05 -0800

    added tag manager; set to infinite posts on service-category Main Query

--- functions.php
+++ functions.php
@@ -162,6 +162,7 @@ function mod_service_categories_order( $query ) {
 		$query->is_tax( 'service-categories' )
 	) :
 		// Set parameters to modify the query
+		$query->set( 'posts_per_page', -1 );
 		$query->set( 'orderby', 'date' );
 		$query->set( 'order', 'ASC' );
 		$query->set( 'suppress_filters', 'true' );
@@ -185,3 +186,26 @@ function redirect_staff() {
 	endif;
 }
 add_action( 'template_redirect', 'redirect_staff' );
+
+function tag_manager_head() {
+	?>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-FVT3DGN');</script>
+	<!-- End Google Tag Manager -->
+	<?php
+}
+add_action('wp_head','tag_manager_head', 20);
+
+function tag_manager_body(){
+	?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-FVT3DGN"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<?php
+}
+add_action('__before_header','tag_manager_body', 20);
