7fab0db035352a585aec02f08398f430cd06909e
xxHiPowerxx <rickyadams432@gmail.com>
2021-05-18 13:47:08 -0700

    moved component-social-media-icon, list and related acf fields to parent-theme; moved xten_render_component() and xten_register_component_id() to parent-theme;

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -21,20 +21,6 @@ function offices_list_shortcode( $atts = '' ) {
 add_shortcode( 'offices_list', 'offices_list_shortcode' );
 
 /**
- * Social Media Icons List Shortcode
- * Renders ALL Social Media Icons Configured on "Site Settings Page"
- */
-function social_media_icons_list_shortcode( $atts = '' ) {
-	// When Shortcode is used $atts defaults to ''.
-	// Ensure that this gets converted to an array.
-	$atts = $atts === '' ? array() : $atts;
-
-	// Get Component Function.
-	return xten_render_component( 'social-media-icons-list' );
-}
-add_shortcode( 'social_media_icons_list', 'social_media_icons_list_shortcode' );
-
-/**
  * Service Categories Shortcode
  * Renders List of ALL Service Categories.
  */
